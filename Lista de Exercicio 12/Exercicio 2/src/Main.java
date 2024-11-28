880b23768453bbb4b265d2a813b5ae84b4c127cf
Lucas Luiz Guesser <lucas.lg17@aluno.ifsc.edu.br>
2024-11-28 10:22:01 -0300

    Mais 2

--- Lista de Exercicio 12/Exercicio 2/src/Main.java
+++ Lista de Exercicio 12/Exercicio 2/src/Main.java
@@ -2,12 +2,12 @@ public class Main {
     public static void main(String[] args) {
 
         String senha = "gasjgsd1213asdsadas32adsas";
-        String digitos = "";
 
-        for (int i = 0; i < senha.length(); i++){
+        for (int i = 0; i < senha.length(); i++) {
             char caractere = senha.charAt(i);
-            if (caractere)
+            if (caractere >= '0' && caractere <= '9') {
+                System.out.print(caractere);
+            }
         }
-
     }
 }
