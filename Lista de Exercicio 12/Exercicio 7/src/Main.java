be0b7c00d3d3abf32a07f6fb75c3e6ec4268a833
Lucas Luiz Guesser <lucas.lg17@aluno.ifsc.edu.br>
2024-11-29 10:13:41 -0300

    Mais 1

--- Lista de Exercicio 12/Exercicio 7/src/Main.java
+++ Lista de Exercicio 12/Exercicio 7/src/Main.java
@@ -5,21 +5,33 @@ public class Main {
 
         Scanner reader = new Scanner(System.in);
 
-        String frase = "casa na casa da casa";
+        System.out.print("Escreva uma frase: ");
+        String input = reader.nextLine();
+        String frase = input.toLowerCase();
 
         int inicio = 0;
         int contagem = 0;
         String palavra;
-        String igual = "casa";
+
+        System.out.print("Escreva uma palavra: ");
+        String input2 = reader.nextLine();
+        String igual = input2.toLowerCase();
 
         for (int i = 0; i < frase.length(); i++){
             if (frase.charAt(i) == ' '){
                 palavra = frase.substring(inicio, i);
-                System.out.println(palavra);
-                if (palavra == igual){
+                boolean eIgual = palavra.equals(igual);
+                if (eIgual == true){
                     contagem++;
                 }
                 inicio = i + 1;
+            }
+            if (i == frase.length() - 1){
+                palavra = frase.substring(inicio, i + 1);
+                boolean eIgual = palavra.equals(igual);
+                if (eIgual == true){
+                    contagem++;
+                }
             }
         }
         System.out.println(contagem);
