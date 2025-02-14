ea3d0a3449e55dd692d2609625b9b33eef7617e9
Lucas Luiz Guesser <lucas.lg17@aluno.ifsc.edu.br>
2025-02-13 21:08:34 -0300

    Mais 1

--- Lista de Exercicio 15/Exercicio 26/src/Main.java
+++ Lista de Exercicio 15/Exercicio 26/src/Main.java
@@ -1,17 +1,22 @@
 public class Main {
     public static void main(String[] args) {
 
-        String palavra = "zyzy";
-        String palavraCifrada = cifraCesar(palavra, 3);
+        String palavra = "xyz";
+        String palavraCifrada = cifraCesar(palavra, -3);
         System.out.print(palavraCifrada);
 
     }
 
     static String cifraCesar(String texto, int deslocamento) {
         String cifrado = "";
+
+        if (deslocamento < 0) {
+            deslocamento = 23;
+        }
+
         for (int i = 0; i < texto.length(); i++) {
             char letra = texto.charAt(i);
-            cifrado += (char) (letra + deslocamento) % 26 + 'a');   
+            System.out.print((char)((letra - 'a' + deslocamento) % 26 + 'a'));
         }
         return cifrado;
     }
